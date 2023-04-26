46d08a21ea3512bd0bc7c0282186e71fb27fb6af
KR Shanto <krshanto2005@gmail.com>
2023-04-26 15:06:11 +0600

    Create NoteBook model

---
+++ models/notebook.ts
@@ -0,0 +1,27 @@
+import mongoose, { Schema } from "mongoose";
+import { NotebookType } from "../types/data/notebook";
+
+const notebookSchema = new Schema<NotebookType>({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  userId: {
+    type: String,
+    required: true,
+  },
+  locked: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Notebook =
+  mongoose.models.Notebook || mongoose.model("Notebook", notebookSchema);
+
+export default Notebook;
