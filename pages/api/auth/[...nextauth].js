818488d41c405bd7f1d1a4b02af1a1b18cdb7c91
KR Shanto <krshanto2005@gmail.com>
2023-04-26 15:08:14 +0600

    Redirect user to /settings/security page after a new user signs up

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,6 +2,8 @@ import NextAuth from "next-auth";
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import GoogleProvider from "next-auth/providers/google";
 import clientPromise from "../../../lib/clientPromise";
+import User from "@/models/user";
+import dbConnect from "@/lib/dbConnect";
 
 export const authOptions = {
   secret: process.env.SECRET,
@@ -15,6 +17,22 @@ export const authOptions = {
         session.session.user._id = session.user.id;
       }
       return session.session;
+    },
+
+    signIn: async (user, account, profile) => {
+      // Check if the user is new or not
+      // Check if there is any `protectionKey` in the user object
+      // If there is no `protectionKey` then redirect to `/settings/security` page
+
+      await dbConnect();
+
+      const userFromDB = await User.findOne({ email: user.user.email });
+
+      if (!userFromDB || !userFromDB.protectionKey) {
+        return Promise.resolve("/settings/security?new=true");
+      }
+
+      return true;
     },
   },
   providers: [
