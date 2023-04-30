ae436a819944bab63f5bb3a283f1f2970f907653
KR Shanto <krshanto2005@gmail.com>
2023-04-30 16:10:08 +0600

    Create api route `get-notes` to get all notes of a notebook

---
+++ pages/api/get-notes.ts
@@ -0,0 +1,65 @@
+import { UserType } from "./../../types/data/user";
+import { NextApiRequest, NextApiResponse } from "next";
+import NoteBook from "@/models/notebook";
+import dbConnect from "@/lib/dbConnect";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import { NextAuthOptions } from "next-auth";
+import response from "@/lib/response";
+import Note from "@/models/note";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  await dbConnect();
+
+  const session = await getServerSession(
+    req,
+    res,
+    authOptions as NextAuthOptions
+  );
+  const sessionUser = session?.user as UserType;
+
+  if (!sessionUser) {
+    return response(res, {
+      type: "UNAUTHORIZED",
+      msg: "You need to be signed in to create a notebook",
+    });
+  }
+
+  const { id, protectionToken } = req.body;
+
+  const book = await NoteBook.findOne({ _id: id, userId: sessionUser._id });
+
+  if (!book) {
+    return response(res, {
+      type: "NOTFOUND",
+      msg: "No notebook found",
+    });
+  }
+
+  if (book.locked && !book.protectionToken) {
+    return response(res, {
+      type: "LOCKED",
+      msg: "No protection token found. You need to unlock the notebook first",
+    });
+  }
+
+  if (book.locked && book.protectionToken !== protectionToken) {
+    return response(res, {
+      type: "LOCKED",
+      msg: "Invalid protection token",
+    });
+  }
+
+  // Get all notes related to the notebook
+  const notes = await Note.find({ notebookId: id }).sort({
+    createdAt: "desc",
+  });
+
+  return response(res, {
+    type: "SUCCESS",
+    data: notes,
+  });
+}
