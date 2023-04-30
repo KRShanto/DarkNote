7fc89d2ca6a8f34207ab633a7de13bde9b6b1213
KR Shanto <krshanto2005@gmail.com>
2023-04-30 16:08:25 +0600

    Create api route `get-books` for getting the notebooks created by the user

---
+++ pages/api/get-books.ts
@@ -0,0 +1,39 @@
+import { UserType } from "./../../types/data/user";
+import { NextApiRequest, NextApiResponse } from "next";
+import NoteBook from "@/models/notebook";
+import dbConnect from "@/lib/dbConnect";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import { NextAuthOptions } from "next-auth";
+import response from "@/lib/response";
+
+// Get all notebooks related to the user
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  await dbConnect();
+
+  const session = await getServerSession(
+    req,
+    res,
+    authOptions as NextAuthOptions
+  );
+  const sessionUser = session?.user as UserType;
+
+  if (!sessionUser) {
+    return response(res, {
+      type: "UNAUTHORIZED",
+      msg: "You need to be signed in to create a notebook",
+    });
+  }
+
+  const books = await NoteBook.find({ userId: sessionUser._id }).sort({
+    createdAt: "desc",
+  });
+
+  return response(res, {
+    type: "SUCCESS",
+    data: books,
+  });
+}
