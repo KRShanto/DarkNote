58295038b0476ccedfabd230a6e1f236749a23dc
KR Shanto <krshanto2005@gmail.com>
2023-06-11 23:08:25 +0600

    Create header (options) in Side nvabar's Notebar

--- components/side-navbar/NotebookBar.tsx
+++ components/side-navbar/NotebookBar.tsx
@@ -1,12 +1,36 @@
 import { useBooksWithNotesStore } from "@/stores/booksWithNotes";
+import { usePopupStore } from "@/stores/popup";
 import React from "react";
 import Book from "./Book";
 
+import { AiFillFileAdd } from "react-icons/ai";
+import { HiFolderPlus } from "react-icons/hi2";
+import { VscCollapseAll } from "react-icons/vsc";
+
 export default function NotebookBar() {
   const { books } = useBooksWithNotesStore();
+  const { openPopup } = usePopupStore();
+
+  function createNote() {
+    openPopup("CreateNote", {});
+  }
 
   return (
     <div className="notebook-bar">
+      <div className="main-header">
+        <h3 className="title">Notebooks</h3>
+        <div className="options">
+          <AiFillFileAdd
+            className="icon"
+            title="New Note"
+            onClick={createNote}
+          />
+          <HiFolderPlus className="icon" title="New Notebook" />
+          <VscCollapseAll className="icon" title="Collapse notebooks" />
+        </div>
+      </div>
+      <hr />
+
       {books.map((book) => (
         <Book key={book._id} book={book} />
       ))}
